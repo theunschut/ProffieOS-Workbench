5d8f267d13c30c80a8b4acf627164c536d8d5414
Theun Schut <t.schut@euphoria-it.nl>
2026-03-18 21:24:35 +0100

    new layout

--- ProffieOS.Workbench/Components/SettingsPanel.razor.cs
+++ ProffieOS.Workbench/Components/SettingsPanel.razor.cs
@@ -1,15 +1,82 @@
 using MudBlazor;
+using ProffieOS.Workbench.Models;
 
 namespace ProffieOS.Workbench.Components;
 
 public partial class SettingsPanel : IDisposable
 {
+    private static readonly HashSet<string> IgnitionBoolVariables =
+    [
+        "gestureon",
+        "swingon",
+        "twiston",
+        "thruston",
+        "stabon"
+    ];
+
+    private static readonly HashSet<string> ActionBoolVariables =
+    [
+        "twistoff",
+        "powerlock",
+        "forcepush"
+    ];
+
+    private static readonly HashSet<string> TimingIntVariables =
+    [
+        "forcepushlen",
+        "lockupdelay"
+    ];
+
+    private static readonly HashSet<string> SensitivityIntVariables =
+    [
+        "swingonspeed",
+        "clashdetect",
+        "maxclash"
+    ];
+
+    private bool HasSystemSettings => State.HasSdToggle;
+    private bool HasBladeSettings => State.HasBrightness || State.HasClashThreshold || State.BladeLengths.Count > 0;
+
+    private IEnumerable<(int Index, BoolSettingItem Setting)> IgnitionBoolSettings => BoolSettings(IgnitionBoolVariables);
+    private IEnumerable<(int Index, BoolSettingItem Setting)> ActionBoolSettings => BoolSettings(ActionBoolVariables);
+    private IEnumerable<(int Index, BoolSettingItem Setting)> OtherBoolSettings => BoolSettingsExcept(IgnitionBoolVariables, ActionBoolVariables);
+
+    private IEnumerable<(int Index, IntSettingItem Setting)> TimingIntSettings => IntSettings(TimingIntVariables);
+    private IEnumerable<(int Index, IntSettingItem Setting)> SensitivityIntSettings => IntSettings(SensitivityIntVariables);
+    private IEnumerable<(int Index, IntSettingItem Setting)> OtherIntSettings => IntSettingsExcept(TimingIntVariables, SensitivityIntVariables);
+
     protected override void OnInitialized()
     {
         State.StateChanged += OnStateChanged;
     }
 
     private void OnStateChanged() => InvokeAsync(StateHasChanged);
+
+    private IEnumerable<(int Index, BoolSettingItem Setting)> BoolSettings(HashSet<string> variables) =>
+        State.GestureBoolSettings
+            .Select((setting, index) => (Index: index, Setting: setting))
+            .Where(x => variables.Contains(x.Setting.Variable));
+
+    private IEnumerable<(int Index, BoolSettingItem Setting)> BoolSettingsExcept(params HashSet<string>[] categories)
+    {
+        var excluded = categories.SelectMany(x => x).ToHashSet(StringComparer.OrdinalIgnoreCase);
+        return State.GestureBoolSettings
+            .Select((setting, index) => (Index: index, Setting: setting))
+            .Where(x => !excluded.Contains(x.Setting.Variable));
+    }
+
+    private IEnumerable<(int Index, IntSettingItem Setting)> IntSettings(HashSet<string> variables) =>
+        State.GestureIntSettings
+            .Select((setting, index) => (Index: index, Setting: setting))
+            .Where(x => variables.Contains(x.Setting.Variable));
+
+    private IEnumerable<(int Index, IntSettingItem Setting)> IntSettingsExcept(params HashSet<string>[] categories)
+    {
+        var excluded = categories.SelectMany(x => x).ToHashSet(StringComparer.OrdinalIgnoreCase);
+        return State.GestureIntSettings
+            .Select((setting, index) => (Index: index, Setting: setting))
+            .Where(x => !excluded.Contains(x.Setting.Variable));
+    }
 
     private async Task SaveSd(bool val)
     {
